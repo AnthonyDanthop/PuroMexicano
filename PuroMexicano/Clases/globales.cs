8694b0df2df3fa9729b89dd48513bc0aae3a8fe4
Danthop S. de R.L. de C.V <danthop@iMac-de-Danthop.lan>
2018-05-11 16:01:46 -0500

    ya corregido el error
    icono y app

--- PuroMexicano/Clases/globales.cs
+++ PuroMexicano/Clases/globales.cs
@@ -201,21 +201,18 @@ namespace PuroMexicano.Clases
             //return mensajeError;
         }
 
-        public static async Task<IDisposable> ToastInfo(string Mensaje)
+        public static void ToastInfo(string Mensaje)
         {
             ToastConfig.DefaultBackgroundColor = System.Drawing.Color.FromArgb(109, 188, 219);
-            var mensajeError = UserDialogs.Instance.Toast(Mensaje);
+            UserDialogs.Instance.Toast(Mensaje);
 
-            return mensajeError;
         }
 
-        public static async Task<IDisposable> success(string Mensaje)
+        public static void success(string Mensaje)
         {
 
             ToastConfig.DefaultBackgroundColor = System.Drawing.Color.FromArgb(56, 166, 84);
-            var mensajeError = UserDialogs.Instance.Toast(Mensaje);
-
-            return mensajeError;
+            UserDialogs.Instance.Toast(Mensaje);
         }
     }
 }
